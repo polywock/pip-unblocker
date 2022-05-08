3835baf9ea474ca0f2da11ac6dcdabc92973ba80
anon <noreply>
2022-05-08 13:34:24 -0400

    fix styling bug

--- popup.js
+++ popup.js
@@ -1,11 +1,29 @@
 
-const standardIcons = {
-  "128": `icon128.png`
-}
+let config;
 
-const grayscaleIcons = {
-  "128": `icon128_grayscale.png`
-}
+window.github.addEventListener("click", e => {
+  window.open("https://github.com/polywock/pipUnblocker", '_blank');
+})
+
+
+window.activate.addEventListener("click", e => {
+  config.enabled = !config.enabled
+  chrome.storage.local.set({config})
+})
+
+chrome.storage.local.get(items => {
+  config = items["config"] || getDefaultConfig()
+  syncDOM()
+  syncIcon()
+})
+
+chrome.storage.onChanged.addListener(changes => {
+  const newConfig = changes["config"]?.newValue
+  if (!newConfig) return 
+  config = newConfig
+  syncDOM()
+  syncIcon()
+})
 
 
 function getDefaultConfig() {
@@ -15,41 +33,12 @@ function getDefaultConfig() {
   }
 }
 
-
-let activateDiv = document.getElementById("activate")
-document.getElementById("github").addEventListener("click", e => {
-  window.open("https://github.com/polywock/pipUnblocker", '_blank');
-})
-
-let config;
-
-activateDiv.addEventListener("click", e => {
-  config.enabled = !config.enabled
-  chrome.storage.local.set({config})
-})
-
-
-
-chrome.storage.local.get(items => {
-  config = items["config"] || getDefaultConfig()
-  syncDOM()
-  syncIcon()
-})
-
-chrome.storage.onChanged.addListener(changes => {
-  const newConfig = changes["config"].newValue
-  if (!newConfig) return 
-  config = newConfig
-  syncDOM()
-  syncIcon()
-})
-
 function syncDOM() {
-  activateDiv.style.color = config.enabled ? "#24c" : "#888"
+  window.activate.style.color = config.enabled ? "#24c" : "#888"
 }
 
 function syncIcon() {
   chrome.browserAction.setIcon({
-    path: config.enabled ? standardIcons : grayscaleIcons
+    path: {"128": config.enabled ? "icon128.png" : "icon128_grayscale.png"} 
   })
 }
